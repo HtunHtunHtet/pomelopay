3ef5f33631d59dc9d661a70a2c0822ca5b184d1f
htunhtunhtet <ryanhhh91@gmail.com>
2020-03-02 09:58:41 +0800

    migration files

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
-1) Clone Repo
-2) Change database credential from .env file
+## DEVELOPMENT ENVIRONMENT
+<ul>
+    <li>PHP 7.2.20</li>
+    <li>MYSQL 5.0.12</li>
+    <li>Symfony 5 </li>
+    <li>Webpack </li> 
+    <li>MAMP environment **local**</li>
+</ul>
+
+## PROJECT INSTALLATION
+
+Clone Repo to your ```htodcs``` if you are using MAMP. 
+```genericsql
+git clone https://github.com/HtunHtunHtet/pomelopay.git
+```
+
+Change directory to the file and update database credential from .env file I am using port 8889 for database. ``APP_ENV=dev`` for development build and ``APP_ENV=prod`` for production build
+```.dotenv
+APP_ENV=dev
+APP_SECRET=c6a89ef3f65c346ebc93aca6908e8a62
+DATABASE_URL=mysql://root:root@127.0.0.1:8889/pomelopay?serverVersion=5.7
+```
+
+
+install composer
+```composer log
+composer install
+```
+
+install yarn 
+```yarn
+yarn install
+```
+
+5) 
 
 
 3) phpunit test =>  ./bin/phpunit
+
+
+http://pomelo.htunhtunhtet.me
---
+++ src/Migrations/Version20200301162500.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200301162500 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE transaction (id INT AUTO_INCREMENT NOT NULL, company_id INT NOT NULL, amount DOUBLE PRECISION NOT NULL, status VARCHAR(255) NOT NULL, currency VARCHAR(255) NOT NULL, INDEX IDX_723705D1979B1AD6 (company_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, name VARCHAR(255) NOT NULL, tax_number VARCHAR(255) NOT NULL, address JSON NOT NULL, UNIQUE INDEX UNIQ_4FBF094FA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1979B1AD6 FOREIGN KEY (company_id) REFERENCES company (id)');
+        $this->addSql('ALTER TABLE company ADD CONSTRAINT FK_4FBF094FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE company DROP FOREIGN KEY FK_4FBF094FA76ED395');
+        $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D1979B1AD6');
+        $this->addSql('DROP TABLE transaction');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE company');
+    }
+}
